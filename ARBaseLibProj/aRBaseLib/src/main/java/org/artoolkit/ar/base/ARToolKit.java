ff5f128804a30a69634c3c82806392488da1b6ae
Norips <raphdruon@gmail.com>
2016-10-17 14:45:01 +0200

    Add fill marker

--- ARBaseLibProj/aRBaseLib/src/main/java/org/artoolkit/ar/base/ARToolKit.java
+++ ARBaseLibProj/aRBaseLib/src/main/java/org/artoolkit/ar/base/ARToolKit.java
@@ -325,6 +325,36 @@ public class ARToolKit {
     }
 
     /**
+     * Retrieves the corner points for the specified marker
+     *
+     * @param markerUID The unique identifier (UID) of the marker to check
+     * @return A float array of size 8 containing the corner points starting at top left (x,y) top right, bottom right, bottom left.
+     *
+     */
+    public float[] queryMarkerCornerPoints(int markerUID) {
+        if (!initedNative) return null;
+        return NativeInterface.arwQueryMarkerCornerPoints(markerUID);
+    }
+
+    /**
+     * Retrieves the nomber of pattern for the specified marker
+     *
+     * @param markerUID The unique identifier (UID) of the marker to check
+     * @return Number of pattern
+     *
+     */
+    public int getMarkerPatternCount(int markerUID) {
+        if (!initedNative) return 0;
+        return NativeInterface.arwGetMarkerPatternCount(markerUID);
+    }
+
+
+    public boolean getMarkerPatternConfig(int markerUID,int patternID, float[] matrix, float[] width, float[] height, int[] imageSizeX, int[] imageSizeY) {
+        if (!initedNative) return false;
+        return NativeInterface.arwGetMarkerPatternConfig(markerUID,patternID,matrix,width,height,imageSizeX,imageSizeY);
+    }
+
+    /**
      * Returns true when video and marker detection are running.
      *
      * @return true when video and marker detection are running, otherwise false
--- ARSimpleProj/aRSimple/src/main/java/org/artoolkit/ar/samples/ARSimple/SimpleRenderer.java
+++ ARSimpleProj/aRSimple/src/main/java/org/artoolkit/ar/samples/ARSimple/SimpleRenderer.java
@@ -49,9 +49,12 @@
 
 package org.artoolkit.ar.samples.ARSimple;
 
+import android.util.Log;
+
 import org.artoolkit.ar.base.ARToolKit;
 import org.artoolkit.ar.base.rendering.ARRenderer;
 import org.artoolkit.ar.base.rendering.Cube;
+import org.artoolkit.ar.base.rendering.Line;
 
 import javax.microedition.khronos.opengles.GL10;
 
@@ -61,9 +64,9 @@ import javax.microedition.khronos.opengles.GL10;
 public class SimpleRenderer extends ARRenderer {
 
     private int markerID = -1;
+    private int markerID2 = -1;
     private Cube cube = new Cube(40.0f, 0.0f, 0.0f, 20.0f);
-    private float gDisplayOrientation = 2.0f; //Portrait
-
+    private Cube cube2 = new Cube(1.0f, 0.0f, 0.0f, 1.0f);
     /**
      * Markers can be configured here.
      */
@@ -71,6 +74,7 @@ public class SimpleRenderer extends ARRenderer {
     public boolean configureARScene() {
 
         markerID = ARToolKit.getInstance().addMarker("nft;Data/pinball");
+        markerID2 = ARToolKit.getInstance().addMarker("nft;Data/003-022");
         if (markerID < 0) return false;
 
         return true;
@@ -102,6 +106,29 @@ public class SimpleRenderer extends ARRenderer {
             gl.glMatrixMode(GL10.GL_MODELVIEW);
             gl.glLoadMatrixf(ARToolKit.getInstance().queryMarkerTransformation(markerID), 0);
             cube.draw(gl);
+        }
+        if (ARToolKit.getInstance().queryMarkerVisible(markerID2)) {
+            int pattID = ARToolKit.getInstance().getMarkerPatternCount(markerID2);
+
+            float[] matrix = new float[16];
+            float[] height = new float[1];
+            float[] width = new float[1];
+            int[] imsX = new int[1];
+            int[] imsY = new int[1];
+
+            if(pattID!=0) {
+                ARToolKit.getInstance().getMarkerPatternConfig(markerID2,pattID-1,matrix,width,height,imsX,imsY);
+                Log.d("Pattern","width = " + width[0] + "\n" +
+                                "height = " + height[0] + "\n" +
+                                "imsX =" + imsX[0] + "\n" +
+                                "imsY =" + imsY[0] + "\n");
+
+            }
+            gl.glMatrixMode(GL10.GL_MODELVIEW);
+            gl.glLoadMatrixf(ARToolKit.getInstance().queryMarkerTransformation(markerID2), 0);
+            gl.glTranslatef(width[0]/2.0f,height[0]/2.0f,1);
+            gl.glScalef(width[0],height[0],1);
+            cube2.draw(gl);
         }
 
     }
