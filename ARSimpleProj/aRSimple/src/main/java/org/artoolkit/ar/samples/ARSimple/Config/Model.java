ed067e461b3087435342d95d8a973af64c316218
Norips <raphdruon@gmail.com>
2016-10-20 11:43:03 +0200

    Add draft class for config

---
+++ ARSimpleProj/aRSimple/src/main/java/org/artoolkit/ar/samples/ARSimple/Config/Model.java
@@ -0,0 +1,15 @@
+package org.artoolkit.ar.samples.ARSimple.Config;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by norips on 20/10/16.
+ */
+
+public class Model {
+    private String name;
+    private float pos[][] = new float[4][3];
+    private ArrayList<String> pathToTextures;
+    private ArrayList<Integer> texture_id;
+}
---
+++ ARSimpleProj/aRSimple/src/main/java/org/artoolkit/ar/samples/ARSimple/Config/Tableau.java
@@ -0,0 +1,14 @@
+package org.artoolkit.ar.samples.ARSimple.Config;
+
+import java.util.ArrayList;
+
+/**
+ * Created by norips on 20/10/16.
+ */
+
+public class Tableau {
+    private String name;
+    private int markerID;
+    private ArrayList<Model> models;
+
+}
