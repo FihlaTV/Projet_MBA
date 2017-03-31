9f62c00e726d5c5c3976ffec57a881dd9d5c5068
Norips <raphdruon@gmail.com>
2017-03-31 10:59:53 +0200

    (feat) Text fill texture

--- ARMuseumProj/ARMuseum/src/main/java/fr/norips/ar/ARMuseum/Drawable/TextureTXT.java
+++ ARMuseumProj/ARMuseum/src/main/java/fr/norips/ar/ARMuseum/Drawable/TextureTXT.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.text.Layout;
 import android.text.StaticLayout;
 import android.text.TextPaint;
@@ -20,28 +21,37 @@ public class TextureTXT extends TextureIMG {
     @Override
         protected Bitmap getBitmapFromAsset(Context context, String text) {
         float scale = context.getResources().getDisplayMetrics().density;
-        Bitmap bitmap = Bitmap.createBitmap(1024, 1024, Bitmap.Config.ARGB_8888);
-// get a canvas to paint over the bitmap
-        Canvas canvas = new Canvas(bitmap);
-        canvas.drawColor(Color.WHITE);
+
+
         TextPaint paint = new TextPaint(Paint.ANTI_ALIAS_FLAG);
         // text color - #3D3D3D
-        paint.setColor(Color.BLACK);
+        paint.setColor(Color.WHITE);
         // text size in pixels
         paint.setTextSize((int) (30 * scale));
         // text shadow
         paint.setShadowLayer(1f, 0f, 1f, Color.WHITE);
         // set text width to canvas width minus 16dp padding
-        int textWidth = canvas.getWidth() - (int) (16 * scale);
+        //int textWidth = canvas.getWidth() - (int) (16 * scale);
 
+        float baseline = -paint.ascent(); // ascent() is negative
+        int width = (int) (paint.measureText(text) + 0.5f); // round
+        int height = (int) (baseline + paint.descent() + 0.5f);
+
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        // get a canvas to paint over the bitmap
+        Canvas canvas = new Canvas(bitmap);
+        canvas.drawColor(Color.TRANSPARENT);
         // init StaticLayout for text
         StaticLayout textLayout = new StaticLayout(
-                text, paint, textWidth, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
+                text, paint, width, Layout.Alignment.ALIGN_NORMAL, 1.0f, 0.0f, false);
 
 
         // get position of text's top left corner
-        float x = (bitmap.getWidth() - textWidth) / 2;
+        float x = (bitmap.getWidth() - width) / 2;
         float y = 0;
+
+
+
 
         // draw text to the Canvas center
         canvas.save();
